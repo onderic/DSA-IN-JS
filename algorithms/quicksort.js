3ad9abdef4214538cfa367b66c678d78036acc33
onderic <ondericollins80@gmail.com>
2023-08-31 10:31:33 +0300

    sorting

---
+++ algorithms/quicksort.js
@@ -0,0 +1,20 @@
+function quickSort(arr){
+    if(arr.length < 2){
+        return arr
+    }
+    let pivot = arr[arr.length-1]
+    left = []
+    right = []
+
+    for(let i = 0; i < arr.length -1; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+const arr = [8,20,-6,4,-2]
+console.log(quickSort(arr))
