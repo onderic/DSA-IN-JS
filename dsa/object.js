be8c98e3fbeacae7977ce795ce51642b09ad0863
onderic <ondericollins80@gmail.com>
2023-09-04 11:12:39 +0300

    array

---
+++ dsa/object.js
@@ -0,0 +1,34 @@
+const obj = {
+    name: 'onderi',
+    age: 24,
+    "key-three": true,
+    sayMyName: function(){
+        console.log(this.name)
+    }
+}
+obj.hobby = "netball"
+delete obj.hobby
+
+
+console.log(obj['age'])
+console.log(obj['key-three'])
+
+
+
+obj.sayMyName()
+
+const keys = Object.keys(obj)
+console.log(keys)
+
+const values = Object.values(obj)
+console.log(values)
+
+const entries = Object.entries(obj)
+console.log(entries)
+
+
+// insert = O(1) constant
+// remove = O(1) constant
+// access = O(1) constant
+// search = O(n) linear
+// object.keys/object.values/object.entries = O(n) linear 
