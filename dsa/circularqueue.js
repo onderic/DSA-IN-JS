ee215714ec4cc82b7793695fc12b5fb8c5d19ee1
onderic <ondericollins80@gmail.com>
2023-09-13 11:19:46 +0300

    CircularQueue

--- dsa/circularqueue.js
+++ dsa/circularqueue.js
@@ -12,4 +12,65 @@ class CircularQueue{
     isEmpty(){
         return this.currentLength === 0
     }
+    enqueue(element){
+        if(!this.isFull()){
+            this.rear = (this.rear + 1) % this.capacity
+            this.items[this.rear] = element
+            this.currentLength += 1
+            if (this.front === -1){
+                this.front = this.rear
+            }
+        }
+    }
+    denqueue(){
+        if(this.isEmpty()){
+            return null
+        }
+        const item = this.items[this.front]
+        this.items[this.front] = null
+        this.front = (this.front + 1) % this.capacity
+        this.currentLength -= 1
+        if(this.isEmpty()){
+            this.front = -1
+            this.rear = -1
+        }
+        return item
+    }
+    peek(){
+        if(!this.isEmpty()){
+            return this.items[this.front]
+        }
+    }
+    print(){
+        if(this.isEmpty()){
+            console.log('The Queue is empty')
+        }else{
+            let i
+            let str =  ''
+            for(i = this.front; i !== this.rear; i = (i+1) %
+            this.capacity){
+                str += this.items[i] +  ''
+            }
+            str += this.items[i]
+            console.log(str)
+        }
+    }
 }
+
+const queue = new CircularQueue(5)
+console.log(queue.isEmpty())
+queue.enqueue(6)
+queue.enqueue(16)
+queue.enqueue(26)
+queue.enqueue(36)
+queue.enqueue(100)
+
+console.log(queue.isFull())
+queue.print()
+
+console.log(queue.denqueue())
+queue.print()
+
+console.log(queue.peek())
+queue.enqueue(120)
+queue.print()
